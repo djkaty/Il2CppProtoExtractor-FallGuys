31774c82575c659341e148c15e80cec73ca17f99
Katy Coe <djkaty@users.noreply.github.com>
2020-08-09 04:44:15 +0200

    Initial commit

---
+++ FallGuysProtoDumper/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FallGuysProtoDumper
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
